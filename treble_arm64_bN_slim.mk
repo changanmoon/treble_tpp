3fa020e1bd3565e27d673ddf90e3bee533784f18
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2024-08-21 09:48:17 +0800

    Initial commit

---
+++ treble_arm64_bN_slim.mk
@@ -0,0 +1,20 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+
+$(call inherit-product, device/phh/treble/pixel.mk)
+
+PRODUCT_NAME := treble_arm64_bN_slim
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MODEL := TrebleDroid
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+PRODUCT_PACKAGES += 
+
