ab2a590e6cb4da200f75bd32afb428b73d38a69e
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2025-05-18 12:58:12 +0800

    Update and rename pixelos_arm64_bgN_erofs.mk to tpp_arm64_bgN_erofs.mk

--- tpp_arm64_bgN_erofs.mk
+++ tpp_arm64_bgN_erofs.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, device/mytja/pixelos/device.mk)
+$(call inherit-product, device/changanmoon/tpp/device.mk)
 
-PRODUCT_NAME := pixelos_arm64_bgN_erofs
+PRODUCT_NAME := tpp_arm64_bgN_erofs
 
 GSI_FILE_SYSTEM_TYPE := erofs
 BOARD_EROFS_COMPRESSOR := lz4hc,9
