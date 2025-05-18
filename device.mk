6b5e874fb29cd3096123483d3069652b29d5620f
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2025-05-18 13:01:17 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/mytja/treble_pixelos/fifteen/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/changanmoon/treble_tpp/15/ota.json
 
 # SELinux
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
