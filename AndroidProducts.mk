8ee60c968b072ee2b6fe16bdd62ee591363cd288
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2025-05-18 12:59:49 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/pixelos_arm64_bgN_erofs.mk \
-	$(LOCAL_DIR)/pixelos_arm64_bgN_ext4.mk \
+	$(LOCAL_DIR)/tpp_arm64_bgN_erofs.mk \
+	$(LOCAL_DIR)/tpp_arm64_bgN_ext4.mk \
